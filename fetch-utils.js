8effa5a876e28ffa27e2dce36b6029a762bd575b
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-22 14:59:45 -0600

    working on fetch-utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,8 +1,12 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://gmpyleofggphhfqygglb.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtcHlsZW9mZ2dwaGhmcXlnZ2xiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE0NTEsImV4cCI6MTk1OTkxNzQ1MX0.aAoG-W_B2pk78Kdb54K8sM3SQbO0g1kbGUOtqvvQhXA';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+
+export function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
 export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
@@ -36,7 +40,9 @@ export async function logout() {
 
     return (window.location.href = '../');
 }
+////////////////////////////////////////////////////////////////////////////////////////////
 
-// function checkError({ data, error }) {
-//     return error ? console.error(error) : data;
-// }
+
+
+
+
