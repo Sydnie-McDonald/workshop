e769080002a34bfdae1720538051e405f26b9dbe
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-22 16:32:28 -0600

    create.js done

--- create/create.js
+++ create/create.js
@@ -1,9 +1,46 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, logout, createParticipant, getWorkshops } from '../fetch-utils.js';
 
 checkAuth();
+
+const form = document.querySelector('form');
 
 const logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const name = data.get('name');
+    const contact = data.get('contact');
+    const workshop_id = data.get('workshop_id');
+
+    // let's make an object to pass as an argument to the createParticipant function
+    await createParticipant({
+        name: name,
+        contact: contact,
+        workshop_id: workshop_id
+    });
+
+    window.location.href = '../workshops';
+});
+
+window.addEventListener('load', async () => {
+    const dropdown = document.querySelector('select');
+
+    const workshops = await getWorkshops();
+
+    for (let workshop of workshops) {
+        const optionEl = document.createElement('option');
+
+        // what the computer sees
+        optionEl.value = workshop.id;
+        // what the user sees
+        optionEl.textContent = workshop.name;
+
+        dropdown.append(optionEl);
+    }
 });
